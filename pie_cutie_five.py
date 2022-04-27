82396a768e98ee4d89acc1f5ac867f6e0266deab
Ztelgin <102710681+Ztelgin@users.noreply.github.com>
2022-04-26 22:39:29 -0500

    Add files via upload

--- pie_cutie_five.py
+++ pie_cutie_five.py
@@ -1,51 +1,132 @@
-# importing libraries
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
 import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtSql import *
 
 
-class Window(QMainWindow):
+class Login(QWidget):
+
     def __init__(self):
         super().__init__()
 
-        # setting title
-        self.setWindowTitle("Login ")
+        self.initUI()
 
-        # setting geometry
-        self.setGeometry(100, 100, 900, 600)
+    def initUI(self):
 
-        # calling method
-        self.UiComponents()
+        self.resize(500,500)
+        self.setWindowTitle('Welcome')
 
-        # showing all the widgets
-        self.show()
+        self.textedit1 = QLineEdit('',self)
+        self.textedit1.move(200,200)
 
-    # method for widgets
-    def UiComponents(self):
+        self.button = QPushButton('Login',self)
+        self.button.move(250,250)
+        self.button.clicked.connect(self.buttonClicked)
 
-        # creating a push button
-        button = QPushButton("CLICK", self)
+    def buttonClicked(self):
 
-        # setting geometry of button
-        button.setGeometry(800, 400, 100, 40)
+        user = self.textedit1.text()
+        self.textedit1.setText('')
 
-
-        # adding action to a button
-        button.clicked.connect(self.clickme)
+        self.f = Floor(self, user)
+        self.f.show()
+        self.close()
 
 
-    # action method
-    def clickme(self):
 
-        # printing pressed
-        print("pressed")
+class Floor(QWidget):
 
-# create pyqt5 app
-App = QApplication(sys.argv)
+    def __init__(self, parent, user):
+        super().__init__()
 
-# create the instance of our Window
-window = Window()
+        self.parent = parent
+        self.user = user
+        self.initUI()
 
-# start the app
-sys.exit(App.exec())
+    def initUI(self):
+
+        grid = QGridLayout()
+        grid.setSpacing(10)
+        self.setLayout(grid)
+        self.resize(500,500)
+
+        names = ['4', '3', '2', '1', '','','23','33',
+                 '12', '', '11', '', '','','22','32',
+                 '', '', '', '', '', '','21','31',
+                 'Return', '', '', '','', '', '', '',
+                 '', '', '', '','', '41', '51', '61',
+                 '', '', '', '','', '42', '52', '62',
+                 '', '', '', '','', '43', '53', '63']
+
+        self.btn_group = QButtonGroup()
+        positions = [(i, j) for i in range(7) for j in range(8)]
+        for position, name in zip(positions, names):
+
+            if name == '':
+                continue
+            button = QPushButton(name)
+            button.name = name
+            self.btn_group.addButton(button)
+            grid.addWidget(button, *position)
+
+        self.btn_group.buttonClicked.connect(self.on_click)
+        self.setWindowTitle('Floor Map - ' + self.user)
+
+    def on_click(self,btn):
+
+        if btn.name == 'Return':
+            self.parent.show()
+            self.close()
+        else:
+            self.table = Table(self, btn.name)
+            self.table.show()
+            self.close()
+
+
+
+class Table(QWidget):
+
+    def __init__(self, parent, number):
+        super().__init__()
+
+        self.parent = parent
+        self.number = number
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.resize(500,500)
+        self.setWindowTitle('Table ' + self.number + ' -' + self.parent.user)
+
+        self.button = QPushButton('Return',self)
+        self.button.move(250,250)
+        self.button.clicked.connect(self.buttonClicked)
+
+    def buttonClicked(self):
+
+        self.parent.show()
+        self.close()
+
+
+
+class Payment(QWidget):
+
+    def __init__(self, parent, number):
+        super().__init__()
+
+
+
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    l = Login()
+    l.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
